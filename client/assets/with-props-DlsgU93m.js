456c7a2805dd36cc65ae532e60f03e8f79b161b4
parkersimon2020 <parkersimon2020@gmail.com>
2025-03-03 21:56:45 -0500

    Updates

---
+++ client/assets/with-props-DlsgU93m.js
@@ -0,0 +1,1 @@
+import{q as p,t,v as e,w as o,a as s,x as u}from"./chunk-HA7DTUK3-B9q-mbWQ.js";function i(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),matches:p()};return s.createElement(a,r)}}function m(a){return function(){const r={params:o(),loaderData:e(),actionData:t(),error:u()};return s.createElement(a,r)}}export{m as a,i as w};
